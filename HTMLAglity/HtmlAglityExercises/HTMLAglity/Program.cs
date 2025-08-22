6bee3dce87cbfb55768eb7004c2de09d9df1bebd
Eia <astyakhossieni@gmail.com>
2025-08-22 15:42:52 +0330

    Build the  Solution & Learn Step1 of HAP

--- HTMLAglity/HtmlAglityExercises/HTMLAglity/Program.cs
+++ HTMLAglity/HtmlAglityExercises/HTMLAglity/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region namespace
+
+// See https://aka.ms/new-console-template for more information
+using HtmlAgilityPack;
+
+#endregion
+
+#region Step 1: Get the HTML Before the Parsing
+
+// initialize the HAP HTTP client
+var web = new HtmlWeb();
+
+// connect to target page
+var document = web.Load("https://www.scrapingcourse.com/ecommerce/");
+
+// print the raw HTML as a string 
+Console.WriteLine(document.DocumentNode.OuterHtml);
+
+// scraping logic...
+
+#endregion
+
+Console.ReadKey();
