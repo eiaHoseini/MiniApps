df98e9c6f69327cccd99329a657d1476cf3dce43
Eia <astyakhossieni@gmail.com>
2025-08-24 15:53:40 +0330

    Add The Person Class For working with ClosedXML

---
+++ HTMLAglity/XMLProject/XMLNoteBook/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLNoteBook.Model
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Gender { get; set; }
+    }
+}
--- HTMLAglity/XMLProject/XMLNoteBook/obj/Debug/net8.0/XMLNoteBook.AssemblyInfo.cs
+++ HTMLAglity/XMLProject/XMLNoteBook/obj/Debug/net8.0/XMLNoteBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XMLNoteBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11d27a881189249807f3c1a223a83f8b29a6e26a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbaaefc5a99f013944addf9adae4744a654dd702")]
 [assembly: System.Reflection.AssemblyProductAttribute("XMLNoteBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XMLNoteBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
